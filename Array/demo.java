b5bb6914ad56e604d03af5e8d5ce2beaa81dd5e2
DESKTOP-BP5MA49\DELL <rahuljio380@gmail.com>
2023-01-22 16:01:49 +0530

    Added name

--- Array/demo.java
+++ Array/demo.java
@@ -3,5 +3,6 @@ import java.util.*;
 public class demo {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Rahul And Shikhar");
     }
 }
