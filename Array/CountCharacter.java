2294d6b9ae08ffab152df6db81c285e8d7568a5a
DESKTOP-BP5MA49\DELL <rahuljio380@gmail.com>
2023-01-22 16:35:05 +0530

    added string question.

---
+++ Array/CountCharacter.java
@@ -0,0 +1,32 @@
+package Array;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string");
+        String str = sc.nextLine();
+        charCount(str);
+    }
+    //RaRhul
+    public static void charCount(String str)
+    {
+        HashMap<Character,Integer> map = new HashMap<>();
+        char st[] = str.toCharArray();
+
+        for (char c: st) {
+            if(map.containsKey(c)){
+                map.put(c, map.get(c)+1);
+            }else {
+                map.put(c,1);
+            }
+
+        }
+        for(Map.Entry entry : map.entrySet()){
+            System.out.println(entry.getKey()+" "+entry.getValue());
+        }
+    }
+}
